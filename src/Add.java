2ecd381ebc99937a39e2e9de14c7cf731cea3bb3
rakshitha <adishesharaogan@gmail.com>
2022-11-10 11:12:27 +0530

    welcome to pythonnnn

--- src/Add.java
+++ src/Add.java
@@ -2,6 +2,7 @@ public class Add{
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        int c=67;
+        System.out.println(a+b+c);
     }
 }
